488d9966e40df4478fc806a3dda01c7734ca7cf1
Daniel Briceño <daniel.b100726@gmail.com>
2022-02-21 17:43:17 -0400

    agregando headers

--- api/app.js
+++ api/app.js
@@ -4,6 +4,31 @@ const express = require("express");
 const multer = require("multer");
 const { google } = require("googleapis");
 const app = express();
+
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+app.name = 'API';
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Range');
+  res.header('Content-Range', 'bytes:0-9/100');
+  next();
+});
 var fileID;
 var dataUrl;
 
